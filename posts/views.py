f28c090f5fb9277401f2c4a2818605db8fe904ad
gilfoyle <sendtosparsh@gmail.com>
2020-05-05 23:27:08 +0545

    bad process button

--- posts/views.py
+++ posts/views.py
@@ -4,9 +4,22 @@ from django.urls import reverse_lazy
 from .forms import PostForm
 from .models import Post
 
+
 class HomePageView(ListView):
 	model = Post
 	template_name = 'posts.html'
+
+
+def process_image(req):
+	form = PostForm(req.POST or None)
+	if form.is_valid():
+		form.save()
+		form = PostForm()
+	context = {
+		'fm': fm
+	}
+	return render(req, 'posts.html', context)
+
 
 class CreatePostView(CreateView):
 	model = Post
