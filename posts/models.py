51b45ae13bd42e85d5a57a1baa55ba37cb9e704b
Narayan <16147528+npadhikari@users.noreply.github.com>
2020-05-18 10:50:30 +0800

    uploading each exam images to different folder

--- posts/models.py
+++ posts/models.py
@@ -1,11 +1,26 @@
 from django.db import models
 
+import os
+
+
+def sheet_upload_location(instance, filename):
+    file_path = 'images/{exam_name}/sheets/{filename}'.format(exam_name=str(instance.title), filename=filename
+                                                              )
+    return file_path
+
+
+def template_upload_location(instance, filename):
+    file_path = 'images/{exam_name}/{filename}'.format(exam_name=str(instance.title), filename=filename
+                                                       )
+    return file_path
+
 
 class Post(models.Model):
-	title = models.TextField()
-	cover = models.ImageField(upload_to = 'images/sheet/')
-	template = models.FileField(upload_to='images/', default=None)
-	marker = models.ImageField(upload_to='images/', null=True, blank=True)
+    title = models.TextField()
+    cover = models.ImageField(upload_to=sheet_upload_location)
+    template = models.FileField(upload_to=template_upload_location, default=None)
+    marker = models.ImageField(upload_to=template_upload_location, null=True, blank=True)
 
-	def __str__(self):
-		return self.title
+
+def __str__(self):
+    return self.title
