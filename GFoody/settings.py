24d2982dea8a665ab4935279662f29e02c37d748
Narayan <16147528+npadhikari@users.noreply.github.com>
2020-05-19 13:45:29 +0800

    delete attachment on removing post

--- GFoody/settings.py
+++ GFoody/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'accounts.apps.AccountsConfig',
-    'posts.apps.PostsConfig'
+    'posts.apps.PostsConfig',
+    'django_cleanup'
 ]
 
 MIDDLEWARE = [
