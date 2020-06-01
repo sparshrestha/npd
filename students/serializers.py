0d4996a8513842de1c5790a60b0f9a1ab5598040
Macbook Pro <macbookpro@Macbooks-MBP-2.domain.name>
2020-06-01 19:12:26 +0545

    revert back

--- students/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import Student
-
-
-class StudentSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Student
-        fields = ('pk', 'first_name', 'last_name', 'email', 'phone', 'address', 'description')
