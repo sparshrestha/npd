586bb58099d1238890455764e29cf78af2edd7b4
Narayan <16147528+npadhikari@users.noreply.github.com>
2020-06-02 14:58:58 +0800

    Update admin.py

--- posts/admin.py
+++ posts/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Post, Student
+from .models import Exams, Student, ProcessedMarks
 
-admin.site.register(Post)
+admin.site.register(Exams)
 admin.site.register(Student)
+admin.site.register(ProcessedMarks)
