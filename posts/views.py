834def252f39f6145662b0edb58512ceeb0cd6fd
Narayan <narayan.adhikari60@gmail.com>
2020-06-14 22:28:06 +0800

    Update views.py

--- posts/views.py
+++ posts/views.py
@@ -9,18 +9,18 @@ from .models import Exams
 
 
 class HomePageView(ListView):
-	model = Exams
-	template_name = 'posts.html'
+    model = Exams
+    template_name = 'posts.html'
 
 
-def process_image(request):
-	print('----------called-------')
-	OMRChecker(input_dir=[BASE_DIR+'/media/images']).execute()
-	return HttpResponseRedirect(reverse('home'))
+def process_image(request, exam_name):
+    print('----------called-------')
+    OMRChecker(input_dir=[BASE_DIR + '/media/images/' + exam_name]).execute()
+    return HttpResponseRedirect(reverse('home'))
 
 
 class CreatePostView(CreateView):
-	model = Exams
-	form_class = ExamsForm
-	template_name = 'post.html'
-	success_url = reverse_lazy('home')
+    model = Exams
+    form_class = ExamsForm
+    template_name = 'post.html'
+    success_url = reverse_lazy('home')
