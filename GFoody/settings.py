70563a86482f4ae14e7d5cb5c12e27ced549321f
Narayan <narayan.adhikari60@gmail.com>
2020-06-18 17:07:12 +0800

    allowed host

--- GFoody/settings.py
+++ GFoody/settings.py
@@ -24,9 +24,7 @@ SECRET_KEY = 'eo)q3v9^+7=m^_p&#b%ty%i%zwfomgoa@3^(0w+p75urfy*f(t'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
-
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
